30e0fa053bdebf729f81961c5e770547c591e713
Jason Gong <jasongong1@outlook.com>
2021-12-21 00:29:02 +1030

    implemented insert preference method, now using async and await in fetch calls so delete happens before insert

--- venv/main.py
+++ venv/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from flask_login import login_required, current_user
 from . import db
 from .models import Preference, Room
@@ -19,17 +19,29 @@ def index():
 @login_required
 def delete_preference():
     req_json = request.get_json()
-    print(req_json['room_name'].strip())
-    print(req_json['rank'].strip())
-    print("test")
-    print("test")
-    print("test")
-    print("test")
     to_delete = Preference.query.join(Room, Preference.room_id==Room.id).filter(
         Room.room_name==req_json['room_name'].strip(),
         Preference.user_zid==current_user.zid,
         Preference.rank==req_json['rank'].strip(),
         ).first()
-    db.session.delete(to_delete)
-    db.session.commit()
+    if (to_delete):
+        db.session.delete(to_delete)
+        db.session.commit()
+        print(f"deleted room name {req_json['room_name'].strip()} in rank {req_json['rank'].strip()}")
     return ('', 204)
+
+@main.route('/insert_preference', methods=['POST'])
+@login_required
+def insert_preference():
+    req_json = request.get_json()
+
+    found_room_id = Room.query.filter_by(room_name=req_json['room_name'].strip()).first().id
+    if (found_room_id):
+        db.session.add(Preference(
+            user_zid=current_user.zid,
+            room_id=found_room_id,
+            rank=int(req_json['rank'])
+        ))
+        db.session.commit()
+        print(f"inserted room name {req_json['room_name'].strip()} in rank {int(req_json['rank'])}")
+    return jsonify(success=True)
--- venv/static/index.js
+++ venv/static/index.js
@@ -7,26 +7,30 @@ window.onload = function() {
             if (true || evt.from == preference_list_sortable) {
                 delete_request(evt.item);
             }
+            insert_request(evt.item, evt.newIndex + 1);
         }
     });
 }
 
-function delete_request(el) {
-    fetch("/delete_preference", {
+async function delete_request(el) {
+    let response = await fetch("/delete_preference", {
         method: "DELETE",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
             'rank': el.cells[0].innerHTML,
             'room_name': el.cells[1].innerHTML
         })
-    })
-    // const xhttp = new XMLHttpRequest();
-    // // console.log(el.cells[0].innerHTML);
-    // // console.log(el.cells[1].innerHTML);
-    // xhttp.open('DELETE', '/delete_preference');
-    // xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    // xhttp.send(JSON.stringify({
-    //     rank: el.cells[0].innerHTML,
-    //     room_name: el.cells[1].innerHTML
-    // }));
+    });
+    // return await response.json();
+}
+
+async function insert_request(el, rank) {
+    await fetch("/insert_preference", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            'rank': rank,
+            'room_name': el.cells[1].innerHTML
+        })
+    });
 }
