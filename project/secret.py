fc1455c150b50d50ca118c5f01eedf5317cc2b6e
Jason Gong <jasongong1@outlook.com>
2022-01-14 23:51:38 +1030

    removed secret.py from gitignore, tracking secret.py in project

---
+++ project/secret.py
@@ -0,0 +1,7 @@
+# git update-index --assume-unchanged secret.py
+# git update-index --no-assume-unchanged secret.py
+# git ls-files -v|grep '^h'
+
+secret_key='super_secret_key'
+auth_email_address=''
+auth_email_password=''
