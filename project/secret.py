5da756bbd4b8d27bee95f787af7c2dea9132ffa3
Jason Gong <jasongong1@outlook.com>
2022-01-14 23:52:38 +1030

    changed commands for assume unchaged in secret to reflect new file struc

--- project/secret.py
+++ project/secret.py
@@ -1,5 +1,5 @@
-# git update-index --assume-unchanged secret.py
-# git update-index --no-assume-unchanged secret.py
+# git update-index --assume-unchanged project/secret.py
+# git update-index --no-assume-unchanged project/secret.py
 # git ls-files -v|grep '^h'
 
 secret_key='super_secret_key'
