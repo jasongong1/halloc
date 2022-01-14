ea2182aed5101a7627deeb6806e67dd45b8bb7a7
Jason Gong <jasongong1@outlook.com>
2022-01-14 23:56:06 +1030

    added password salt field to secret

--- project/__init__.py
+++ project/__init__.py
@@ -9,6 +9,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = secret_key
+    app.config['SECRET_KEY'] = secret_key
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rooms.db'
 
     db.init_app(app)
