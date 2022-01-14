7aefadf563751b83391c1f2f00c603e777efd10e
Jason Gong <jasongong1@outlook.com>
2022-01-14 23:56:23 +1030

    added password salt field to secret

--- project/secret.py
+++ project/secret.py
@@ -2,6 +2,7 @@
 # git update-index --no-assume-unchanged project/secret.py
 # git ls-files -v|grep '^h'
 
-secret_key='super_secret_key'
+secret_key=''
+secret_password_salt=''
 auth_email_address=''
 auth_email_password=''
