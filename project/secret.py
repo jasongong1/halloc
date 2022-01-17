a68fd49e572f79c8fea142033269c91a9e1381ef
Jason Gong <jasongong1@outlook.com>
2022-01-17 22:46:29 +1030

    deleting old filed before updating project

--- project/secret.py
+++
@@ -1,7 +0,0 @@
-# git update-index --assume-unchanged project/secret.py
-# git update-index --no-assume-unchanged project/secret.py
-# git ls-files -v|grep '^h'
-
-secret_key=''
-security_password_salt=''
-auth_email_address=''
